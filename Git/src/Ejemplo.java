23b44fff6b6b3a762a9808eeed7b958c25b00226
Karat <Karat@DESKTOP-LFJN6VL>
2023-03-05 19:45:55 +0100

    Comienzo del programa

---
+++ Git/src/Ejemplo.java
@@ -0,0 +1,11 @@
+
+public class Ejemplo {
+
+	public static void main(String[] args) {
+		int numero = 0;
+
+		System.out.println("Hola mi gente");
+		System.out.println(numero);
+	}
+
+}
---
+++ Git/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Karat
+ *
+ */
+module Git {
+}
